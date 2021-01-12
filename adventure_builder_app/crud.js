4dad9a85944138d77fce86803f6d2e9498bf32e2
aroseman <whoisroseman@gmail>
2021-01-12 13:52:40 -0500

    CRUD template

--- adventure_builder_app/crud.js
+++ adventure_builder_app/crud.js
@@ -3,11 +3,43 @@ const config = require('./settings.json')
 
 const uri = "mongodb+srv://" + config.username + ":" + config.password + "@adventurebuilder.3wnez.mongodb.net/AdventureBuilder?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    if(err) {
-        return console.log(err)
-    }
-  const collection = client.db("test").collection("devices");
-  console.log(collection)
-  client.close();
-});
+
+function CreateDocument() {
+    client.connect(err => {
+        if(err) {
+            return console.log(err);
+        }
+      const db = client.db(config.database);
+      client.close();
+    });
+}
+
+function ReadDocument() {
+    client.connect(err => {
+        if(err) {
+            return console.log(err);
+        }
+        const db = client.db(config.database);
+        client.close();
+    })
+}
+
+function UpdateDocument() {
+    client.connect(err => {
+        if(err) {
+            return console.log(err);
+        }
+        const db = client.db(config.database);
+        client.close();
+    })
+}
+
+function DeleteDocument() {
+    client.connect(err => {
+        if(err) {
+            return console.log(err);
+        }
+        const db = client.db(config.database);
+        client.close();
+    })
+}
