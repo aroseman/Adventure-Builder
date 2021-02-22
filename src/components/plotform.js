77a7527c7dc3a6f46ff926a46c933d2143c234fa
Andrew Roseman <amr6163@psu.edu>
2021-02-21 21:17:15 -0500

    Dungeon Goals and Villains implemented

--- src/components/plotform.js
+++ src/components/plotform.js
@@ -7,15 +7,18 @@ class PlotForm extends Component {
         this.state = {
             plotName: "",
             plotLocation: "",
+            dungeonGoal: "",
+            villain: "",
         }
         this.handleClick = this.handleClick.bind(this);
         this.handlePlotNameChange = this.handlePlotNameChange.bind(this);
     }
     handleClick(e) {
-        fetch('http://localhost:4000/get-plots')
-            .then(response => { return response.json()})
-            .then(data => {this.setState({ plotName: data[0].name})})
-        console.log(this.state.plotName)
+        // fetch('http://localhost:4000/get-plots')
+        //     .then(response => { return response.json()})
+        //     .then(data => {this.setState({ plotName: data[0].name})})
+        this.getDungeonGoal(this.rollDie())
+        this.getVillains(this.rollDie())
     }
     handlePlotNameChange(e) {
         this.setState({plotName: e.target.value});
@@ -23,9 +26,29 @@ class PlotForm extends Component {
     handlePlotLocationChange(e) {
         this.setState({plotLocation: e.target.value});
     }
+    handleDungeonGoalChange(e) {
+        this.setState({dungeonGoal: e.target.value});
+    }
+    handleVillainChange(e) {
+        this.setState({villain: e.target.value});
+    }
+    rollDie() {
+        return Math.floor(Math.random() * 20) + 1
+    }
+    getDungeonGoal(rollValue) {
+        fetch('http://localhost:4000/get-dungeon-goals')
+            .then(response => {return response.json()})
+            .then(data => {this.setState({dungeonGoal: data[0][rollValue]})})
+    }
+    getVillains(rollValue) {
+        fetch('http://localhost:4000/get-villain')
+            .then(response => {return response.json()})
+            .then(data => {this.setState({villain: data[0][rollValue]})})
+    }
     render() {
         return (
-            <form>
+            <div>
+            <form id="requirements">
                 <div class="text-field edit-plot-name">
                     <label for="plot-name">Plot Name</label>
                     <input type="text" id="plot-name" name="plot-name" value={this.state.plotName} onChange={this.handlePlotNameChange}/>
@@ -46,6 +69,16 @@ class PlotForm extends Component {
                     <input name="roll-plot" type="button" class="die-btn" value="" onClick = {this.handleClick}/>
                 </div>
             </form>
+
+            <form id="plot">
+                <label for="dungeon-goal">Dungeon Goal</label>
+                <textarea id="dungeon-goal" name="dungeon-goal" value={this.state.dungeonGoal} onChange={this.handleDungeonGoalChange}/>
+                <div class="textfield edit-villain">
+                    <label for="villain">Villain</label>
+                    <input type="text" id="villain" name="villain" value={this.state.villain} onChange={this.handleVillainChange}/>
+                </div>
+            </form>
+            </div>
         )
     }
 }
